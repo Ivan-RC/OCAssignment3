a18409c208564233ecf270a7aec38f822f27a7f1
Ivan Rivas <IRivas1@my.gcu.edu>
2019-02-22 15:13:56 -0700

    Update

--- OCAssignment3/Form1.Designer.cs
+++ OCAssignment3/Form1.Designer.cs
@@ -28,13 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn1_Display = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btn1_Clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn1_Display
+            // 
+            this.btn1_Display.Location = new System.Drawing.Point(289, 391);
+            this.btn1_Display.Name = "btn1_Display";
+            this.btn1_Display.Size = new System.Drawing.Size(75, 31);
+            this.btn1_Display.TabIndex = 0;
+            this.btn1_Display.Text = "Display";
+            this.btn1_Display.UseVisualStyleBackColor = true;
+            this.btn1_Display.Click += new System.EventHandler(this.btn1_Display_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.Black;
+            this.richTextBox1.ForeColor = System.Drawing.Color.Lime;
+            this.richTextBox1.Location = new System.Drawing.Point(85, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(558, 373);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btn1_Clear
+            // 
+            this.btn1_Clear.Location = new System.Drawing.Point(370, 391);
+            this.btn1_Clear.Name = "btn1_Clear";
+            this.btn1_Clear.Size = new System.Drawing.Size(75, 31);
+            this.btn1_Clear.TabIndex = 2;
+            this.btn1_Clear.Text = "Clear";
+            this.btn1_Clear.UseVisualStyleBackColor = true;
+            this.btn1_Clear.Click += new System.EventHandler(this.btn1_Clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn1_Clear);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btn1_Display);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn1_Display;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btn1_Clear;
     }
 }
 
--- OCAssignment3/Form1.cs
+++ OCAssignment3/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace OCAssignment3
 {
@@ -15,6 +16,73 @@ namespace OCAssignment3
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btn1_Display_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    System.IO.StreamReader sr = new System.IO.StreamReader(openFileDialog1.FileName);
+                    string longestWord = "";
+                    string allText = sr.ReadToEnd();
+                    richTextBox1.Text = allText.ToLower();
+                    string[] text = allText.ToLower().Split();
+                    string firstText = text[0];
+                    string lastText = text[0];
+                    int maxVowels = 0;
+                    string mostVowels = "";
+
+                    for (int i = 0; i < text.Length; i++)
+                    {
+                        //Find first and last word alphabetically
+                        if (text[i].CompareTo(firstText) < 0)
+                            firstText = text[i];
+                        if (text[i].CompareTo(lastText) > 0)
+                            lastText = text[i];
+
+                        //Find longest word
+                        if (text[i].Length > longestWord.Length)
+                        {
+                            longestWord = text[i];
+                        }
+
+                        //Find word with most vowels
+                        int vowelCt = 0;
+                        for(int j = 0; j < text[i].Length; j++)
+                        {
+                            if (text[i][j] == 'a' ||
+                                text[i][j] == 'e' ||
+                                text[i][j] == 'i' ||
+                                text[i][j] == 'o' ||
+                                text[i][j] == 'u')
+                            {
+                                vowelCt++;
+                            }
+                        }
+                        if (vowelCt > maxVowels)
+                        {
+                            maxVowels = vowelCt;
+                            mostVowels = text[i];
+                        }
+                    }
+
+                    richTextBox1.Text += "\nThis is the last word: " + firstText + ". This is the first word: " + lastText;
+                    richTextBox1.Text += "\nThis is the longest word: " + longestWord;
+                    richTextBox1.Text += "\nThis is the word with the most vowels: " + mostVowels;
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show (ex.Message);
+            }
+        }
+
+        private void btn1_Clear_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = " ";
         }
     }
 }
